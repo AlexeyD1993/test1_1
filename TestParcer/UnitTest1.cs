154aa385e6de40118fa98c52e1e5f084ad9df2b1
Alexey <alexeyD1993@yandex.ru>
2021-03-15 21:22:48 +0300

    - start fix unit test

--- TestParcer/UnitTest1.cs
+++ TestParcer/UnitTest1.cs
@@ -11,14 +11,14 @@ namespace TestParcer
         {
             //Arrange
             HttpResult httpReq = new HttpResult();
-            httpReq.Url = "http://google.com";
+            httpReq.Url = "http://google.com/";
             httpReq.RequestBody = "{\"order\":\"123\"," +
                     "\"name\":\"qwe\"," +
                     "\"pass\":\"12qw\"}";
             httpReq.ResponseBody = "ok";
 
             HttpResult httpResult = new HttpResult();
-            httpResult.Url = "http://google.com";
+            httpResult.Url = "http://google.com/";
             httpResult.RequestBody = "{\"order\":\"123\"," +
                     "\"name\":\"XXX\"," +
                     "\"pass\":\"XXXX\"}";
@@ -29,7 +29,7 @@ namespace TestParcer
             httpReq = secureCleaner.CleanString(httpReq);
 
             //Assert
-            Assert.False(httpResult == httpReq);
+            Assert.True(httpResult == httpReq);
         }
 
         [Fact]
@@ -37,13 +37,13 @@ namespace TestParcer
         {
             //Arrange
             HttpResult httpReq = new HttpResult();
-            httpReq.Url = "http://google.com";
+            httpReq.Url = "http://google.com/";
             httpReq.RequestBody = "ok";
             httpReq.ResponseBody = "{\"order\":{\"name\":\"qwe\"," +
                     "\"pass\":\"12qw\"}}";
 
             HttpResult httpResult = new HttpResult();
-            httpResult.Url = "http://google.com";
+            httpResult.Url = "http://google.com/";
             httpResult.RequestBody = "ok";
             httpResult.ResponseBody = "{\"order\":{\"name\":\"XXX\"," +
                     "\"pass\":\"XXXX\"}}";
@@ -53,7 +53,7 @@ namespace TestParcer
             httpReq = secureCleaner.CleanString(httpReq);
 
             //Assert
-            Assert.False(httpResult == httpReq);
+            Assert.True(httpResult == httpReq);
         }
 
         [Fact]
@@ -75,7 +75,7 @@ namespace TestParcer
             httpReq = secureCleaner.CleanString(httpReq);
 
             //Assert
-            Assert.False(httpResult == httpReq);
+            Assert.True(httpResult == httpReq);
         }
 
         [Fact]
@@ -97,7 +97,59 @@ namespace TestParcer
             httpReq = secureCleaner.CleanString(httpReq);
 
             //Assert
-            Assert.False(httpResult == httpReq);
+            Assert.True(httpResult == httpReq);
+        }
+
+        [Fact]
+        public void SecureCleaner_CleanString_XMLResponceBody_OK()
+        {
+            //Arrange
+            HttpResult httpReq = new HttpResult();
+            httpReq.Url = "http://google.com";
+            httpReq.RequestBody = "ok";
+            httpReq.ResponseBody = "<main>" +
+                                        "<order>123</order><name>max</name><pass>qwe12</pass>" +
+                                      "</main>";
+
+            HttpResult httpResult = new HttpResult();
+            httpResult.Url = "http://google.com";
+            httpResult.RequestBody = "ok";
+            httpResult.ResponseBody = "<main>" +
+                                        "<order>123</order><name>XXX</name><pass>XXXXX</pass>" +
+                                      "</main>";
+
+            //Act
+            SecureCleaner secureCleaner = new SecureCleaner();
+            httpReq = secureCleaner.CleanString(httpReq);
+
+            //Assert
+            Assert.True(httpResult == httpReq);
+        }
+
+        [Fact]
+        public void SecureCleaner_CleanString_XMLRequestBody_OK()
+        {
+            //Arrange
+            HttpResult httpReq = new HttpResult();
+            httpReq.Url = "http://google.com";
+            httpReq.RequestBody = "<main>" +
+                                        "<order><name>max</name><pass>qwe12</pass></order>" +
+                                      "</main>";
+            httpReq.ResponseBody = "ok";
+
+            HttpResult httpResult = new HttpResult();
+            httpResult.Url = "http://google.com";
+            httpResult.RequestBody = "<main>" +
+                                        "<order></order><name>XXX</name><pass>XXXXX</pass></order>" +
+                                      "</main>";
+            httpResult.ResponseBody = "ok";
+
+            //Act
+            SecureCleaner secureCleaner = new SecureCleaner();
+            httpReq = secureCleaner.CleanString(httpReq);
+
+            //Assert
+            Assert.True(httpResult == httpReq);
         }
     }
 }
