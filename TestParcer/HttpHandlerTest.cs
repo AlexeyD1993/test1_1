abbbd0944653f9fd0fbeca665790cb9a47aa81e9
Alexey <alexeyD1993@yandex.ru>
2021-03-17 20:49:53 +0300

    - add test 1.2

---
+++ TestParcer/HttpHandlerTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using test1_1;
+using Xunit;
+
+namespace TestParcer
+{
+    public class HttpHandlerTest
+    {
+        [Fact]
+        public void HttpLogHandler_Process_BookingcomHttpResult_ClearSecureData()
+        {
+            //Arrange
+            var bookingcomHttpResult = new HttpResult
+            {
+                Url = "http://test.com/users/max/info?pass=123456",
+                RequestBody = "http://test.com?user=max&pass=123456",
+                ResponseBody = "http://test.com?user=max&pass=123456"
+            };
+            var httpLogHandler = new HttpHandler();
+
+
+            //Act
+            httpLogHandler.Process(bookingcomHttpResult);
+
+            //Assert
+            Assert.Equal("http://test.com/users/XXX/info?pass=XXXXXX", httpLogHandler.CurrentLog.Url);
+            Assert.Equal("http://test.com?user=XXX&pass=XXXXXX", httpLogHandler.CurrentLog.RequestBody);
+            Assert.Equal("http://test.com?user=XXX&pass=XXXXXX", httpLogHandler.CurrentLog.ResponseBody);
+        }
+
+    }
+}
---
+++ test1_1/HttpHandler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace test1_1
+{
+    public class HttpHandler
+    {
+        HttpResult _currentLog;
+        public HttpResult CurrentLog { get { return _currentLog; } }
+
+        public string Process(HttpResult httpResultProcess)
+        {
+            var httpResult = new HttpResult
+            {
+                Url = httpResultProcess.Url,
+                RequestBody = httpResultProcess.RequestBody,
+                ResponseBody = httpResultProcess.ResponseBody
+            };
+
+            //очищаем secure данные в httpResult, либо создаем новый clearedHttpResult на основе httpResult
+            httpResult = new SecureCleaner().CleanString(httpResult);
+
+            Log(httpResult);
+
+            return httpResultProcess.ResponseBody;
+        }
+
+        /// <summary>
+        /// Логирует данные запроса, они должны быть уже без данных которые нужно защищать 
+        /// </summary>
+        /// <param name="result"></param>
+        protected void Log(HttpResult result)
+        {
+            _currentLog = new HttpResult
+            {
+                Url = result.Url,
+                RequestBody = result.RequestBody,
+                ResponseBody = result.ResponseBody
+            };
+        }
+    }
+
+}
